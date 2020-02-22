87b9169a21b6a6f2bbbed5d588e8382fca3d5cb0
Ahmad <ahmad2smile@protonmail.com>
2020-02-22 20:34:36 +0500

    styled Repos table with public css assets

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as Hapi from "@hapi/hapi";
+import Path from "path";
 
 import { routes } from "./routes";
 import { plugins } from "./plugins";
@@ -7,9 +8,27 @@ import { views } from "./views";
 const init = async (): Promise<void> => {
 	const port = process.env.PORT || 3005;
 
-	const server = new Hapi.Server({ port });
+	const server = new Hapi.Server({
+		port,
+		routes: {
+			files: {
+				relativeTo: Path.join(__dirname, "public")
+			}
+		}
+	});
 
 	await server.register(plugins);
+
+	server.route({
+		method: "GET",
+		path: "/{param*}",
+		handler: {
+			directory: {
+				path: ".",
+				redirectToSlash: true
+			}
+		}
+	});
 
 	server.route(routes);
 
